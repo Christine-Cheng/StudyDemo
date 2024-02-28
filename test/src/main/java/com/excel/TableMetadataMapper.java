2d7e9e664d551e1ece2bda77b67d2093af3c3ffc
Happy <atom164318791856@gmail.com>
2024-02-28 22:25:00 +0800

    20240228 导出表结构到excel并且设置excel表的格式字体等

---
+++ test/src/main/java/com/excel/TableMetadataMapper.java
@@ -0,0 +1,17 @@
+package com.excel;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Describe:
+ * @Author Happy
+ * @Create 2024-02-27 10:22:02
+ **/
+public interface TableMetadataMapper {
+    
+    List<TableMetadataDTO> getTableMetadata(Map<String, Object> params);
+    
+    List<String> getTables(String databaseName);
+    
+}
