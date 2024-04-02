7fecb6e99a911273394be7acf51ccf89a0ca22f3
Happy <atom164318791856@gmail.com>
2024-04-02 13:45:38 +0800

    20240402 策略模式附件上传

--- test/src/main/java/com/variousdemo/design_patterns/strategy/AttachController.java
+++ test/src/main/java/com/variousdemo/design_patterns/strategy/AttachController.java
@@ -1,7 +1,6 @@
 package com.variousdemo.design_patterns.strategy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Create 2024-04-02 11:18:25
  **/
 @Slf4j
-@PostMapping(value = "uploadAttach", name = "上传附件")
 @RestController
 public class AttachController {
 
