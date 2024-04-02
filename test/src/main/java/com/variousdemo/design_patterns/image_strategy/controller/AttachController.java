946f9bcc03f01801cba881df7b6d6c83bd4c4e78
Happy <1737575350@qq.com>
2024-04-02 23:51:29 +0800

    202400403 更新

--- test/src/main/java/com/variousdemo/design_patterns/image_strategy/controller/AttachController.java
+++ test/src/main/java/com/variousdemo/design_patterns/image_strategy/controller/AttachController.java
@@ -1,6 +1,7 @@
 package com.variousdemo.design_patterns.image_strategy.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Create 2024-04-02 11:18:25
  **/
 @Slf4j
+@RequestMapping(value = "/uploadAttach", name = "附件上传")
 @RestController
 public class AttachController {
 
